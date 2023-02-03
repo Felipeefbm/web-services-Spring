d10aca21dc9c38b7ad353b77a90e3a428abe4fe6
Elvys Felipe <efbm009@gmail.com>
2023-02-02 21:25:22 -0300

    OService layer, component registration

--- src/main/java/com/unforgiven/course/entityes/User.java
+++ src/main/java/com/unforgiven/course/entityes/User.java
@@ -1,11 +1,14 @@
 package com.unforgiven.course.entityes;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,18 +17,22 @@ public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id												
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> order = new ArrayList<>();  
+	
 	public User() {
 	}
 	
 	public User(Long id, String name, String email, String phone, String password) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -72,6 +79,11 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrder() {
+		return order;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -97,6 +109,8 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
+
+
 	
 	
 	
